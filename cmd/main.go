0138752ab9a76e6199ad670d4721db104ef25c20
victormg <victor.mejia@clubhub.ai>
2025-02-02 13:34:04 -0500

    feat(root): consumer v1

---
+++ cmd/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/IBM/sarama"
+)
+
+func ConnectConsumer(brokers []string) (sarama.Consumer, error) {
+	config := sarama.NewConfig()
+	config.Producer.Return.Errors = true
+
+	return sarama.NewConsumer(brokers, config)
+
+}
+func main() {
+	// Do something
+	topic := "price"
+	msgCount := 0
+
+	worker, err := ConnectConsumer([]string{"localhost:9092"})
+	if err != nil {
+		panic(err)
+	}
+
+	consumer, err := worker.ConsumePartition(topic, 0, sarama.OffsetNewest)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to Kafka")
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	doneCh := make(chan struct{})
+
+	go func() {
+		for {
+			select {
+			case err := <-consumer.Errors():
+				fmt.Println(err)
+			case msg := <-consumer.Messages():
+				msgCount++
+				fmt.Println("Recive order cound %d: topic=%s, partition=%d, offset=%d, key=%s, value=%s", msgCount, msg.Topic, msg.Partition, msg.Offset, string(msg.Key), string(msg.Value))
+				fmt.Println("Received message", string(msg.Value))
+				time.Sleep(200 * time.Millisecond)
+			case <-sigchan:
+				doneCh <- struct{}{}
+			}
+		}
+	}()
+	<-doneCh
+	println("Messages processed: ", msgCount)
+
+	if err := worker.Close(); err != nil {
+		panic(err)
+	}
+
+}
---
+++ internal/domain/entity/price_history.go
@@ -0,0 +1,25 @@
+package entity
+
+import gonanoid "github.com/matoous/go-nanoid/v2"
+
+type ProductHistoryEntity struct {
+	ID          string  `gorm:"primaryKey"`
+	ProductId   string  `gorm:"column:product_id"`
+	Price       float64 `gorm:"column:price"`
+	OrderClosed string  `gorm:"column:order_closed"`
+}
+
+func NewProductHistoryEntity(
+	productId string,
+	price float64,
+	orderClosed string,
+) ProductHistoryEntity {
+	uid, _ := gonanoid.New(20)
+
+	return ProductHistoryEntity{
+		ID:          uid,
+		ProductId:   productId,
+		Price:       price,
+		OrderClosed: orderClosed,
+	}
+}
